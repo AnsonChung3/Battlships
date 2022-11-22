e84cdf62230342131b69f3fdd19c0c708abe2c2f
Anson Chung <>
2022-11-22 18:25:04 +0000

    Added test for q-tab / q-tab-panel

--- src/pages/DraftGame2.vue
+++ src/pages/DraftGame2.vue
@@ -1,6 +1,25 @@
 <template>
     <div>
         <h1>{{ sampleText }}</h1>
+        <q-card>
+            <q-tabs
+                v-model="tab"
+                active-color="bg-negative"
+                class="bg-primary text-secondary"
+                dense
+            >
+                <q-tab name="Manual" label="Manual" />
+                <q-tab name="Auto" label="Auto" />
+            </q-tabs>
+            <q-tab-panels v-model="tab" class="bg-info">
+                <q-tab-panel name="Manual">
+                    <div class="bg-info text-secondary">Mails</div>
+                </q-tab-panel>
+                <q-tab-panel name="Auto">
+                    <div class="bg-info text-secondary">Auto</div>
+                </q-tab-panel>
+            </q-tab-panels>
+        </q-card>
         <div class="row">
             <left-player class="col"></left-player>
             <left-player class="col"></left-player>
@@ -11,6 +30,8 @@
 <script setup>
 import { ref } from 'vue';
 import LeftPlayer from 'src/components/LeftPlayer.vue';
+
+const tab = ref('mails');
 
 const sampleText = ref(`2nd Draft of Game`);
 
