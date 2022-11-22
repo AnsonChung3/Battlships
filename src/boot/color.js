def6de52ebe43bea4e7a648e7d5127887a8521af
Anson Chung <>
2022-11-22 18:22:26 +0000

    Added custom branded color

--- src/boot/color.js
+++ src/boot/color.js
@@ -3,4 +3,6 @@ import { setCssVar } from 'quasar';
 export default () => {
     setCssVar('primary', '#D4D4D4');
     setCssVar('secondary', '#1E1E1E');
+    setCssVar('negative', '#929292');
+    setCssVar('info', '#B2B2B2');
 };
