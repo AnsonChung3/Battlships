4f4b6629fafdffd12640e18ec2b6d3f4053f1b17
Anson Chung <>
2023-02-09 22:54:36 +0000

    Removed prop passing into child component

--- src/components/CustomQBtn.vue
+++ src/components/CustomQBtn.vue
@@ -1,13 +1,6 @@
 <template>
     <q-btn
-        :label=props.labelText
         color="primary"
         text-color="secondary"
     />
 </template>
-
-<script setup>
-const props = defineProps({
-    labelText: String
-});
-</script>
