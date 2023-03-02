8afbc399b588e361ba5c664633535b79e775a470
Anson Chung <>
2023-03-02 17:42:37 +0000

    Rough set up for Pinia store. Only states are set up for now

---
+++ src/stores/battleship.js
@@ -0,0 +1,26 @@
+import { defineStore } from 'pinia'
+
+export const useBattleshipStore = defineStore('battleship', () => {
+    const p1GridArray = ref([])
+    const p1ShipsArray = ref([])
+    
+    const p2GridArray = ref([])
+    const p2ShipsArray = ref([])
+
+    const gridWidth = 10
+    const shipLengths = [5, 4, 3, 3, 2, 2, 1, 1]
+    const STATES = {
+        BLANK: 0,
+        PLACED: 1,
+        MARGIN: 2,
+        HIT: 3,
+        MISS: 4
+    }
+    const COLORS = {
+        BLANK: '948C15',
+        PLACED: '1F9415',
+        MARGIN: '13468C',
+        HIT: '921313',
+        MISS: '383232'
+    }
+})
