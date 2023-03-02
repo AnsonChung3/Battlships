79d0d1cb24ecc0cc46290566784795f90850f8c4
Anson Chung <>
2023-03-02 15:47:21 +0000

    Rough set up done

---
+++ src/components/GamePanel.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <!-- game panel is created once both players have confirmed placement
+        would be on at all times, except showing different info
+        for when it's showing as home/opponent panel -->
+        <div v-for="(row, R) in gridArray" :key="R">
+            <div class="inline" v-for="(cell, C) in row" :key="C">
+                <div
+                    v-if="homePanel"
+                    class="cell"
+                    :style="{background: '#'+cellColor(R, C)}"
+                >
+                    ID: {{ cell.ID }}
+                    P: {{ cell.placement }}
+                </div>
+                <!-- cells are only clickable when it's showing as opponent panel -->
+                <div
+                    v-else
+                    @click="isAttackLand(R, C)"
+                    class="cell"
+                    :style="{background: '#'+cellColor(R, C)}"
+                >
+                    ID: {{ cell.ID }}
+                    P: {{ cell.placement }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+// const gridArray
+// props.homePanel
+// function isAttackLand
+</script>
+
+<style scoped>
+/* inline
+cell */
+</style>
