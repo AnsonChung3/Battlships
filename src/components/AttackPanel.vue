df335a5db98c64db7e4887baf7b1a5dd1b5cc01d
Anson Chung <>
2023-03-06 14:36:03 +0000

    Added homePanel. Roughly set up with props and store. Tidied up cellColor( )

--- src/components/AttackPanel.vue
+++ src/components/AttackPanel.vue
@@ -6,6 +6,7 @@
         <div v-for="(row, R) in gridArray" :key="R">
             <div class="inline" v-for="(cell, C) in row" :key="C">
                 <div
+                    v-if="homePanel"
                     class="cell"
                     :style="{background: '#'+cellColor(R, C)}"
                 >
@@ -13,7 +14,7 @@
                     P: {{ cell.placement }}
                 </div>
                 <!-- cells are only clickable when it's showing as opponent panel -->
-                <!-- <div
+                <div
                     v-else
                     @click="isAttackLand(R, C)"
                     class="cell"
@@ -21,7 +22,7 @@
                 >
                     ID: {{ cell.ID }}
                     P: {{ cell.placement }}
-                </div> -->
+                </div>
             </div>
         </div>
     </div>
@@ -32,76 +33,66 @@ import { toRef } from 'vue';
 
 import { useBattleshipStore } from 'stores/battleship.js';
 const store = useBattleshipStore();
+const shipsArray = store.shipsArray;
 const STATES = store.STATES;
 const COLORS = store.COLORS;
 
 // gridArray should later be modded to another prop?
 const gridArray = toRef(store, 'gridArray');
 
-// const props = defineProps({
-//     home: Boolean
-// });
-// homePanel = toRef(props, 'home');
 // homePanel needs to be props
+const props = defineProps({
+    home: Boolean
+});
+const homePanel = toRef(props, 'home');
 
-// cellColor can be further modded to with only home/opponent view
 function cellColor (R, C) {
     const cell = gridArray.value[R][C];
-    if (cell.placement === STATES.PLACED) {
-        if (cell.isHit) {
-            return COLORS.HIT;
+    if (homePanel.value === true) {
+        if (cell.placement === STATES.PLACED) {
+            if (cell.isHit) {
+                return COLORS.HIT;
+            } else {
+                return COLORS.PLACED;
+            }
         } else {
-            return COLORS.PLACED;
+            if (cell.isHit) {
+                return COLORS.MISS;
+            } else {
+                return COLORS.BLANK;
+            }
         }
     } else {
-        if (cell.isHit) {
-            return COLORS.MISS;
-        } else {
+        if (!cell.isHit) {
             return COLORS.BLANK;
+        } else if (cell.placement === STATES.PLACED) {
+            return COLORS.HIT;
+        } else {
+            return COLORS.MISS;
         }
     }
-    // this stretch of comment is basically the finished version of coloring logic
-    // delete the top after homePanel is set up
-    // if (homePanel.value === true) {
-    //     if (cell.placement === STATES.PLACED) {
-    //         if (cell.isHit) {
-    //             return COLORS.HIT;
-    //         } else {
-    //             return COLORS.PLACED;
-    //         }
-    //     } else {
-    //         if (cell.isHit) {
-    //             return COLORS.MISS;
-    //         } else {
-    //             return COLORS.BLANK;
-    //         }
-    //     }
-    // } else {
-    //     if (!cell.isHit) {
-    //         return COLORS.BLANK;
-    //     } else if (cell.placement === STATES.PLACED) {
-    //         return COLORS.HIT;
-    //     } else {
-    //         return COLORS.MISS;
-    //     }
-    // }
 }
 
 // dependent methods for isAttackLand: isDestroyed, isEnd
-// function isAttackLand (R, C) {
-//     const checkCell = gridArray.value[R][C];
-//     checkCell.isHit = true;
-//     if (checkCell.placement === STATES.PLACED && isDestroyed(checkCell.ID)) {
-//         shipsArray.value[checkCell.ID - 1].isSunk = true;
-//         console.log(`ship ID ${checkCell.ID} is destroyed`);
-//         const isEnd = shipsArray.value.every(ship => ship.isSunk);
-//         if (isEnd) {
-//             console.log(`emit end from ${props.player}`);
-//             emit('game-end');
-//         }
-//     }
-// no need to emit anymore? as it can take turn by changing p1Active from anywhere
+function isAttackLand (R, C) {
+    const checkCell = gridArray.value[R][C];
+    checkCell.isHit = true;
+    if (checkCell.placement === STATES.PLACED && isDestroyed(checkCell.ID)) {
+        shipsArray.value[checkCell.ID - 1].isSunk = true;
+        console.log(`ship ID ${checkCell.ID} is destroyed`);
+        const isEnd = shipsArray.value.every(ship => ship.isSunk);
+        if (isEnd) {
+            console.log(`emit end from ${props.player}`);
+            // emit('game-end');
+        }
+    }
+//     no need to emit anymore? as it can take turn by changing p1Active from anywhere
 //     emit('shoot');
-// }
+}
+
+function isDestroyed (ID) {
+    const cells = gridArray.value.flat().filter(cell => cell.ID === ID);
+    return (cells.every(cell => cell.isHit));
+}
 
 </script>
