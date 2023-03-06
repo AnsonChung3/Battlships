1b8d6bb9255017cc5add46f84fbcae1549c0d4a0
Anson Chung <>
2023-03-06 14:56:43 +0000

    Set up v-if logic for divs

--- src/pages/DraftGame3.vue
+++ src/pages/DraftGame3.vue
@@ -1,18 +1,31 @@
 <template>
     <div>
-        <div class="row">
-            <div class="col">
-                <placement-header/>
+        <div>
+            <div v-if="!confirm.p1" class="row">
+                <placement-header class="col" />
+                <div class="col"> <h2>!confirm.p1</h2> </div>
             </div>
-            <div class="col">
-                <attack-header/>
-                <attack-panel/>
+            <div v-else-if="!confirm.p2" class="row">
+                <div class="col"> !confirm.p2 </div>
+                <placement-header class="col" />
+            </div>
+            <div v-else class="row">
+                <div class="col">
+                    <attack-header/>
+                    <attack-panel/>
+                </div>
+                <div class="col">
+                    <attack-header/>
+                    <attack-panel/>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
+
 import AttackHeader from 'components/AttackHeader.vue';
 import AttackPanel from 'components/AttackPanel.vue';
 import PlacementHeader from 'components/PlacementHeader.vue';
@@ -21,6 +34,8 @@ import { useBattleshipStore } from 'stores/battleship.js';
 const store = useBattleshipStore();
 store.generateGrid();
 store.generateShipsArray();
+
+const confirm = ref({ p1: false, p2: false });
 
 </script>
 
