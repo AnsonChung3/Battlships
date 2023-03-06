ed403e4a0c267f1f0402c40fd237ea5b59ad4f95
Anson Chung <>
2023-03-06 14:36:15 +0000

    Rough set up done

--- src/components/PlacementPanel.vue
+++ src/components/PlacementPanel.vue
@@ -2,9 +2,18 @@
     <div>
         <div v-for="(row, R) in gridArray" :key="R">
             <div class="inline" v-for="(cell, C) in row" :key="C">
-                <!-- cells are clickable only when in manual mode -->
+                <!-- cells are only for display when in auto mode -->
                 <div
-                    v-if="!placementConfirmed"
+                    v-if="auto"
+                    class="cell"
+                    :style="{background: '#'+cellColor(R, C)}"
+                >
+                    ID: {{ cell.ID }}
+                    P: {{ cell.placement }}
+                </div>
+                <!-- cells are clickable only when in manual mode -->
+                <!-- <div
+                    v-else
                     @click="manualValidation(R, C, manualID, manualPlaceRight)"
                     @mouseover="hoverState({R, C})"
                     class="cell"
@@ -12,19 +21,38 @@
                 >
                     ID: {{ cell.ID }}
                     P: {{ cell.placement }}
-                </div>
+                </div> -->
             </div>
         </div>
     </div>
 </template>
 
 <script setup>
+import { toRef } from 'vue';
+const props = defineProps({
+    auto: Boolean,
+    gridArray: Array
+});
+const auto = toRef(props, 'auto');
+const gridArray = toRef(props, 'gridArray');
+import { useBattleshipStore } from 'stores/battleship.js';
+const store = useBattleshipStore();
+const STATES = store.STATES;
+const COLORS = store.COLORS;
+
+function cellColor (R, C) {
+    const cell = gridArray.value[R][C];
+    if (cell.placement === STATES.PLACED) {
+        return COLORS.HIT;
+    }
+    if (cell.placement === STATES.MARGIN) {
+        return COLORS.MARGIN;
+    } else {
+        return COLORS.BLANK;
+    }
+}
 // gridArray
 // placementConfirmed
 // manualvalidation
 // cellColoe
 </script>
-
-<style scoped>
-/* inline */
-</style>
